442be5d7a33431db10eaffcb8cfc139fa32a36c1
stefaniemartins <stefanie.m@aluno.ifsc.edu.br>
2019-03-25 14:31:36 -0300

    Iniciando repositório.

---
+++ src/poo/Batedeira.java
@@ -0,0 +1,45 @@
+package poo;
+
+public class Batedeira
+{
+    // Atributos
+    private int velocidade; // 0 (desligada) até 5 (máxima).
+    private int pote; // 1 - pequeno, 2 - médio e 3 - grande.
+    private String pa;
+
+    // Métodos
+    public String toString()
+    {
+        return "Batedeira velocidade = " + velocidade + ",pote = " + pote + ", pá = " + pa;
+    }
+
+    public int getVelocidade()
+    {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade)
+    {
+        this.velocidade = velocidade;
+    }
+
+    public int getPote()
+    {
+        return pote;
+    }
+
+    public void setPote(int pote)
+    {
+        this.pote = pote;
+    }
+
+    public String getPa()
+    {
+        return pa;
+    }
+
+    public void setPa(String pa)
+    {
+        this.pa = pa;
+    }
+}
---
+++ src/poo/Principal.java
@@ -0,0 +1,13 @@
+package poo;
+
+public class Principal
+{
+    public static void main(String[] args)
+    {
+        Batedeira arno = new Batedeira();
+        Batedeira wallita = new Batedeira();
+
+        System.out.println(arno);
+        System.out.println(wallita.toString());
+    }
+}
